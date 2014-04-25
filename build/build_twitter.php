eadf96c825fae81185119d5d4d6ab8c3f2491ab7
Cal Henderson <cal@iamcal.com>
2014-04-25 10:37:38 -0700

    building twitter/hangouts sheets

---
+++ build/build_twitter.php
@@ -0,0 +1,54 @@
+<?php
+	# load positional data
+	include('catalog_positions.php');
+
+
+
+	create_alt_sheet(72, 'twitter');
+
+
+	function create_alt_sheet($img_w, $type){
+
+		global $position_data;
+
+
+		$img_path = "../img-{$type}-{$img_w}/";
+
+		$max_x = 0;
+		$max_y = 0;
+		foreach ($position_data as $pos){
+			$max_x = max($max_x, $pos['x']);
+			$max_y = max($max_y, $pos['y']);
+		}
+
+		$pw = ($max_x+1) * $img_w;
+		$ph = ($max_y+1) * $img_w;
+
+
+
+		echo "Compositing images ... ";
+		$dst = dirname(__FILE__)."/../sheet_{$type}_{$img_w}.png";
+
+		echo "convert -size {$pw}x{$ph} null: -matte -compose Clear -composite -compose Over {$dst}";
+exit;
+
+		echo shell_exec("convert -size {$pw}x{$ph} null: -matte -compose Clear -composite -compose Over {$dst}");
+
+		foreach ($position_data as $k => $pos){
+
+			$src = "{$img_path}{$k}.png";
+			if (!file_exists($src)) contrinue;
+
+			$px = $pos['x'] * $img_w;
+			$py = $pos['y'] * $img_w;
+
+			echo shell_exec("composite -geometry +{$px}+{$py} {$src} {$dst} {$dst}");
+			echo '.';
+		}
+
+		echo " DONE\n";
+
+		echo "Optimizing sheet   ... ";
+		echo shell_exec("convert {$dst} png32:{$dst}");
+		echo "DONE\n";
+	}
