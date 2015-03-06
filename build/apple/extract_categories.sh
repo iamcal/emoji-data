465594b89ff1b5a0dd727a145919b761e1f9d53f
Cal Henderson <cal@iamcal.com>
2015-03-05 16:40:07 -0800

    double quotes are required to work here

--- build/apple/extract_categories.sh
+++ build/apple/extract_categories.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 plist="/System/Library/Input Methods/CharacterPalette.app/Contents/Resources/Category-Emoji.plist"
 outfile=../emoji_categoires.json
-plutil -convert json -r '$plist' -o '$outfile'
+plutil -convert json -r "$plist" -o "$outfile"
