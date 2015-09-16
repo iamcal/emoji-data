16790bf328431f149367cccbf9d2defb633db03d
Cal Henderson <cal@iamcal.com>
2015-09-16 11:55:57 -0700

    when finding flags, also look at twitter/google/emojione images. updated emoji_data.txt to include all flags.

--- build/find_flags.php
+++ build/find_flags.php
@@ -3,28 +3,43 @@
 	# this script builds a list of all the flags we have images for
 	#
 
-	$files = glob("../img-apple-64/1f1*-1f1*.png");
+	$a = find_flags("../img-apple-64/1f1*-1f1*.png");
+	$b = find_flags("../img-google-64/1f1*-1f1*.png");
+	$c = find_flags("../img-twitter-64/1f1*-1f1*.png");
+	$d = find_flags("../img-emojione-64/1f1*-1f1*.png");
 
-	$cps = array();
-	foreach ($files as $file){
-		$bits = explode('/', $file);
-		$bits = explode('.', array_pop($bits));
-		$cp = StrToUpper(array_shift($bits));
-		$cps[] = $cp;
-	}
+	$all = array_unique(array_merge($a, $b, $c, $d));
+	sort($all);
 
-	sort($cps);
+	foreach ($all as $line) echo $line;
 
-	foreach ($cps as $cp){
+	function find_flags($path){
 
-		if (!strlen($cp)) continue;
+		$files = glob($path);
 
-		list($a, $b) = explode('-', $cp);
-		$a = hexdec($a) - 0x1F1E6;
-		$b = hexdec($b) - 0x1F1E6;
+		$cps = array();
+		foreach ($files as $file){
+			$bits = explode('/', $file);
+			$bits = explode('.', array_pop($bits));
+			$cp = StrToUpper(array_shift($bits));
+			$cps[] = $cp;
+		}
 
-		$a = chr(ord('a') + $a);
-		$b = chr(ord('a') + $b);
+		$out = array();
 
-		echo "$cp;flag-$a$b\n";
+		foreach ($cps as $cp){
+
+			if (!strlen($cp)) continue;
+
+			list($a, $b) = explode('-', $cp);
+			$a = hexdec($a) - 0x1F1E6;
+			$b = hexdec($b) - 0x1F1E6;
+
+			$a = chr(ord('a') + $a);
+			$b = chr(ord('a') + $b);
+
+			$out[] = "$cp;flag-$a$b\n";
+		}
+
+		return $out;
 	}
