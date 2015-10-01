f2aec43384e4f9c11d666e15699e62fa0d0eba74
Cal Henderson <cal@iamcal.com>
2015-10-01 14:44:59 -0700

    fixed the naming of the emojione family ligatures to match the apple way (200D joiners)

--- build/emojione/get64.php
+++ build/emojione/get64.php
@@ -2,10 +2,21 @@
 	$src = "../../emojione/assets/png";
 	$dst = "../../img-emojione-64";
 
+	shell_exec("rm -f ../../img-emojione-64/*.png");
+
 	$files = glob("{$src}/*.png");
 	foreach ($files as $file){
 		$base = basename($file);
-		$dst_file = $dst.'/'.StrToLower($base);
+		$target = StrToLower($base);
+
+		if (preg_match('!^(1f468|1f469)-!', $target)){
+			$parts = explode('-', $target);
+			if (count($parts) > 2){
+				$target = implode('-200d-', $parts);
+			}
+		}
+
+		$dst_file = $dst.'/'.$target;
 		copy($file, $dst_file);
 		echo '.';
 	}
