b065c92ececb3979043346d0805bf9571348e6e9
Cal Henderson <cal@iamcal.com>
2017-03-09 21:14:59 -0800

    modified facebook grab script to pull down messenger images

--- build/facebook/grab.php
+++ build/facebook/grab.php
@@ -1,47 +1,106 @@
 <?php
+
 $json = file_get_contents('../../emoji.json');
 $data = json_decode($json, true);
-shell_exec("rm -f ../../img-facebook-128/*.png");
+
+#shell_exec("rm -f ../../img-messenger-128/*.png");
+#shell_exec("rm -f ../../img-facebook-128/*.png");
+
+
 foreach ($data as $row){
-	if (strlen($row['image'])) fetch($row['image']);
+	if (strlen($row['image'])){
+		fetch($row['image']);
+	}
+
 	if (isset($row['skin_variations'])){
 		foreach ($row['skin_variations'] as $row2){
 			fetch($row2['image']);
 		}
 	}
 }
+
 function fetch($img){
+	fetch_single($img, 'MESSENGER', 'img-messenger-128', 128);
+	#fetch_single($img, 'FBEMOJI', 'img-facebook-72', 72);
+}
+
+
+function fetch_single($img, $type_key, $dir, $size){
 	/* based upon Twitter scraper and Javascript from Facebook see Github issue #56 */
+
+	# files get stored here
+	$path = "../../{$dir}/{$img}";
+
+	# use this for just fetching missing images
+	if (file_exists($path)) return;
 	
-	/* Emoji Config */
-	$pixelRatio = 1;
-	$schemaAuth = "https://www.facebook.com/images/emoji.php/v7";
-	$fileExt = ".png";
+	# Emoji Config
 	$supportedSizes = [16, 18, 20, 24, 28, 30, 32, 64, 128];
 	$types = array('FBEMOJI' => 'f', 'FB_EMOJI_EXTENDED' => 'e', 'MESSENGER' => 'z', 'UNICODE' => 'u');
-	/** hardcoded defaults: */
-	$size = 128; // default = highest available
-	$type = $types['FBEMOJI'];
+
+	$type = $types[$type_key];
+
+	# try the simple version
+
+	$url = build_url($type, $size, $img);
 	
-	$size = in_array($size, $supportedSizes) ? $size : 128;
-	$path = $pixelRatio . '/' . $size . '/' . $img . $fileExt;
+	if (try_fetch($url, $path)){
+		echo '.';
+		return;
+	}
+
+
+	# now try with underscores
+
+	$url = build_url($type, $size, str_replace('-', '_', $img));
+
+	if (try_fetch($url, $path)){
+		echo '.';
+		return;
+	}
+
+	echo "X[{$img}]";
+}
+
+
+function build_url($type, $size, $img){
+
+	$pixelRatio = 1;
+	$schemaAuth = "https://www.facebook.com/images/emoji.php/v7";
+
+	$path = $pixelRatio . '/' . $size . '/' . $img;
 	$check = checksum($path);
-	$url_img = $schemaAuth . '/' . $type . $check . '/' . $path;
+	$url = $schemaAuth . '/' . $type . $check . '/' . $path;
 	
-	$path = "../../img-facebook-128/{$img}"; /** files get stored here */
-	$url = $schemaAuth . $url_img; /** files get retrieved from here */
-	#echo "{$img} - $path - $url\n";
-	#continue;
+	return $url;
+}
+
+function try_fetch($url, $path){
+
 	$out = array();
 	$ret = 0;
 	exec("wget -qO {$path} \"{$url}\"", $out, $ret);
+
+	# error from wget?
 	if ($ret){
-		echo "X[{$img}]";
 		#print_r($out);
-		if (file_exists($path) && !filesize($path)) unlink($path);
-	}else{
-		echo ".";
+		@unlink($path);
+		return false;
 	}
+
+	# PNG signature?
+	$fp = fopen($path, 'r');
+	$sig = fread($fp, 4);
+	fclose($fp);
+
+	if ($sig != "\x89PNG"){
+		#print_r($out);
+		@unlink($path);
+		return false;
+	}
+
+	# ok!
+	return true;
 }
 function encodeURIComponent($str) { /* a standard method in Javascript */
 	return $str;
@@ -60,4 +119,3 @@ function checksum($subpath) {
 	}
 	return base_convert(($base & 255), 10, 16);
 }
-?>
--- build/facebook/make64.php
+++ build/facebook/make64.php
@@ -1,16 +1,21 @@
 <?php
-	$files = glob("../../img-facebook-128/*.png");
-	shell_exec("rm -f ../../img-facebook-64/*.png");
-	foreach ($files as $src){
-		$bits = explode('/', $src);
-		$dst = '../../img-facebook-64/'.array_pop($bits);
-		exec("convert {$src} -resize 64x64 png32:{$dst}", $out, $code);
-		if ($code){
-			echo "ERROR:\n";
-			echo "   ".$out."\n";
-		}else{
-			echo ".";
+	thumb_set('img-messenger-128', 'img-messenger-64');
+
+
+	function thumb_set($from, $to){
+
+		$files = glob("../../{$from}/*.png");
+		shell_exec("rm -f ../../{$to}/*.png");
+		foreach ($files as $src){
+			$bits = explode('/', $src);
+			$dst = "../../{$to}/".array_pop($bits);
+			exec("convert {$src} -resize 64x64 png32:{$dst}", $out, $code);
+			if ($code){
+				echo "ERROR:\n";
+				echo "   ".$out."\n";
+			}else{
+				echo ".";
+			}
 		}
+		echo "All done\n";
 	}
-	echo "All done\n";
-?>
