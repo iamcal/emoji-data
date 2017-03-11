839514d1a29f199cacab3d39ec0436a8d9800b5b
Cal Henderson <cal@iamcal.com>
2017-03-10 17:58:21 -0800

    * only fetch with underscores instead of dashes * use curl for fetching * pass referer and user agent headers

--- build/facebook/grab.php
+++ build/facebook/grab.php
@@ -3,6 +3,7 @@
 $json = file_get_contents('../../emoji.json');
 $data = json_decode($json, true);
 
+# comment these out to do incremental download
 #shell_exec("rm -f ../../img-messenger-128/*.png");
 #shell_exec("rm -f ../../img-facebook-128/*.png");
 
@@ -40,17 +41,9 @@ function fetch_single($img, $type_key, $dir, $size){
 
 	$type = $types[$type_key];
 
-	# try the simple version
 
-	$url = build_url($type, $size, $img);
-	
-	if (try_fetch($url, $path)){
-		echo '.';
-		return;
-	}
-
-
-	# now try with underscores
+	# emoji.img = acbd-1234.png
+	# facebook  = abcd_1234.png
 
 	$url = build_url($type, $size, str_replace('-', '_', $img));
 
@@ -59,7 +52,9 @@ function fetch_single($img, $type_key, $dir, $size){
 		return;
 	}
 
-	echo "X[{$img}]";
+	#echo "X[{$img}]";
+	echo 'X';
+	#sleep(1);
 }
 
 
@@ -77,13 +72,13 @@ function build_url($type, $size, $img){
 
 function try_fetch($url, $path){
 
-	$out = array();
-	$ret = 0;
-	exec("wget -qO {$path} \"{$url}\"", $out, $ret);
+	http_fetch($url, $path);
 
-	# error from wget?
-	if ($ret){
-		#print_r($out);
+	if (!file_exists($path)){
+		return false;
+	}
+
+	if (!filesize($path)){
 		@unlink($path);
 		return false;
 	}
@@ -94,7 +89,7 @@ function try_fetch($url, $path){
 	fclose($fp);
 
 	if ($sig != "\x89PNG"){
-		#print_r($out);
+		#print_r($sig);
 		@unlink($path);
 		return false;
 	}
@@ -118,4 +113,52 @@ function checksum($subpath) {
 		$base &= 4294967295;
 	}
 	return base_convert(($base & 255), 10, 16);
+}
+
+
+function http_fetch($url, $filename){
+
+	$fh = fopen($filename, 'w');
+
+	$options = array(
+		CURLOPT_FILE	=> $fh,
+		CURLOPT_TIMEOUT	=> 60,
+		CURLOPT_URL	=> $url,
+	);
+
+	$options[CURLOPT_HTTPHEADER] = array(
+		'Referer: https://www.facebook.com/',
+		'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+	);
+
+	$ch = curl_init();
+	curl_setopt_array($ch, $options);
+	curl_exec($ch);
+	$ret = curl_getinfo($ch);
+	curl_close($ch);
+
+	fclose($fh);
+
+	# show http error code?
+	#echo "({$ret['http_code']})";
+
+	#print_r($ret);
+	#exit;
+}
+
+
+function http_fetch_old($url, $filename){
+
+	$out = array();
+	$ret = 0;
+	exec("wget -qO {$filename} \"{$url}\"", $out, $ret);
+
+	# error from wget?
+	if ($ret){
+		#print_r($out);
+		@unlink($path);
+		return false;
+	}
+
+	return true;
 }
