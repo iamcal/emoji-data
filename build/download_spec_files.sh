12d290da2cee68136b324ce7495514bd9f17405a
Cal Henderson <cal@iamcal.com>
2017-03-10 19:58:43 -0800

    update versions of the data we're pulling down - Unicode 9.0.0 and Emoji 4.0

--- build/download_spec_files.sh
+++ build/download_spec_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget -O unicode/UnicodeData.txt http://www.unicode.org/Public/UNIDATA/UnicodeData.txt
-wget -O unicode/emoji-data.txt http://www.unicode.org/Public/emoji/2.0/emoji-data.txt
-wget -O unicode/emoji-sequences.txt http://www.unicode.org/Public/emoji/2.0/emoji-sequences.txt
-wget -O unicode/emoji-zwj-sequences.txt http://www.unicode.org/Public/emoji/2.0/emoji-zwj-sequences.txt
+wget -O unicode/UnicodeData.txt http://www.unicode.org/Public/9.0.0/ucd/UnicodeData.txt
+wget -O unicode/emoji-data.txt http://www.unicode.org/Public/emoji/4.0/emoji-data.txt
+wget -O unicode/emoji-sequences.txt http://www.unicode.org/Public/emoji/4.0/emoji-sequences.txt
+wget -O unicode/emoji-zwj-sequences.txt http://www.unicode.org/Public/emoji/4.0/emoji-zwj-sequences.txt
 
