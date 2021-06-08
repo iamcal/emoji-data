008ea35776d43e47e233a7e872251f464ef1beaf
Cal Henderson <cal@iamcal.com>
2021-06-08 22:01:04 +0000

    more testing

--- build/apple/lig.pl
+++ build/apple/lig.pl
@@ -24,9 +24,16 @@ $f->{'morx'}->read();
 
 #print $f->{'morx'}->resolve_ligature([0x1f1ea, 0x1f1f8])."\n"; # spanish flag
 #print $f->{'morx'}->resolve_ligature([0x1f1ea, 0x1f1f9])."\n"; # some other flag
-#print $f->{'morx'}->resolve_ligature([0x1f3ca, 0x1f3ff])."\n"; # swimmer, black skin
+###print Dumper $f->{'morx'}->resolve_ligature([0x1f3ca, 0x1f3ff]); # swimmer, black skin
 
 #print $f->{'morx'}->resolve_ligature([0x1f469, 0x200d, 0x1f469, 0x200d, 0x1f467])."\n";
-#print $f->{'morx'}->resolve_ligature([0x1f469, 0x200d, 0x1f469, 0x200d, 0x1f467, 0x200d, 0x1f466])."\n";
+# print Dumper $f->{'morx'}->resolve_ligature([0x1f469, 0x200d, 0x1f469, 0x200d, 0x1f467, 0x200d, 0x1f466]);
 
 print Dumper $f->{'morx'}->resolve_ligature([0x1F468, 0x200D, 0x2764, 0xFE0F, 0x200D, 0x1F468]); # man-heart-man
+print Dumper $f->{'morx'}->resolve_ligature([0x1F469, 0x1F3FB, 0x200D, 0x1F91D, 0x200D, 0x1F468, 0x1F3FB]); # man_and_woman_holding_hands (tone)
+#print Dumper $f->{'morx'}->resolve_glyph_list([1264, 921]);
+
+#print Dumper $f->{'morx'}->resolve_ligature([0x1F469, 0x1F3FC, 0x1F91D, 0x1F468, 0x1F3FC]); # man_and_woman_holding_hands (tone)
+print Dumper $f->{'morx'}->resolve_ligature([0x1F469, 0x1F3FB]); # man_and_woman_holding_hands (tone)
+
+#$f->{'morx'}->test_features();
