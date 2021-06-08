16e385135fcad147662f54de65392b6f6a2c3286
Cal Henderson <cal@iamcal.com>
2021-06-08 03:24:29 +0000

    show better summary and compare multiple files at once

--- build/apple/tables.pl
+++ build/apple/tables.pl
@@ -18,23 +18,44 @@ use Font::TTF::TTC;
 $Font::TTF::Font::tables{'sbix'} = 'Font::TTF::Sbix';
 $Font::TTF::Font::tables{'morx'} = 'Font::TTF::Morx';
 
-my $filename = "apple_color_emoji_11_4.ttc";
-$filename = "apple_color_emoji_10_15_1.ttc";
+&scan_tables("apple_color_emoji_10_15_1.ttc");
+&scan_tables("apple_color_emoji_11_4.ttc");
 
-my $f = Font::TTF::TTC->openCollection($filename) || die "Unable to read $filename : $!";
-$f->readCollection(0);
 
-my $morx = $f->{'morx'};
+sub scan_tables {
 
-$morx->read();
+	my ($filename) = @_;
 
-for my $chain_id(0..scalar(@{$morx->{'header'}->{'chains'}})-1){
-	print "scanning chain ${chain_id}...\n";
+	my $f = Font::TTF::TTC->openCollection($filename) || die "Unable to read $filename : $!";
+	$f->readCollection(0);
 
-	for my $subtable_id(0..scalar(@{$morx->{'header'}->{'chains'}->[$chain_id]->{'subtables'}})-1){
+	my $morx = $f->{'morx'};
+	$morx->read();
 
-		my $sub = $morx->{'header'}->{'chains'}->[$chain_id]->{'subtables'}->[$subtable_id];
+	my $sizes = {};
 
-		print "  subtable ${subtable_id}, type=$sub->{'type'}, length=$sub->{'length'}\n";
+	for my $chain_id(0..scalar(@{$morx->{'header'}->{'chains'}})-1){
+		#print "scanning chain ${chain_id}...\n";
+
+		for my $subtable_id(0..scalar(@{$morx->{'header'}->{'chains'}->[$chain_id]->{'subtables'}})-1){
+
+			my $sub = $morx->{'header'}->{'chains'}->[$chain_id]->{'subtables'}->[$subtable_id];
+
+			#print "  subtable ${subtable_id}, type=$sub->{'type'}, length=$sub->{'length'}\n";
+
+			$sizes->{$sub->{'type'}} += $sub->{'length'};
+		}
 	}
+
+	#print Dumper $sizes;
+
+	print "$filename\n";
+	print "    0: (Indic) Rearrangement subtable -> $sizes->{0}\n" if $sizes->{0};
+	print "    1: Contextual subtable            -> $sizes->{1}\n" if $sizes->{1};
+	print "    2: Ligature subtable              -> $sizes->{2}\n" if $sizes->{2};
+	print "    3: RESERVED subtable              -> $sizes->{3}\n" if $sizes->{3};
+	print "    4: Noncontextual (Swash) subtable -> $sizes->{4}\n" if $sizes->{4};
+	print "    5: Insertion subtable             -> $sizes->{5}\n" if $sizes->{5};
+	print "\n";
+
 }
