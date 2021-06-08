5f7206cbd50f3b39d2afc34a1a693fcbeb025516
Cal Henderson <cal@iamcal.com>
2021-06-08 16:58:37 +0000

    got the ligature debug tool working

--- build/apple/lig.pl
+++ build/apple/lig.pl
@@ -7,14 +7,16 @@ use lib '../lib';
 use Font::TTF::Font;
 use Font::TTF::Sbix;
 use Font::TTF::Morx;
+use Font::TTF::TTC;
 
 $Font::TTF::Font::tables{'sbix'} = 'Font::TTF::Sbix';
 $Font::TTF::Font::tables{'morx'} = 'Font::TTF::Morx';
 
-my $filename = "apple_color_emoji_10_10_3.ttf";
+my $filename = "apple_color_emoji_11_4.ttc";
+#$filename = "apple_color_emoji_10_15_5.ttc";
 
-my $f = Font::TTF::Font->open($filename) || die "Unable to read $filename : $!";
-
+my $f = Font::TTF::TTC->openCollection($filename) || die "Unable to read $filename : $!";
+$f->readCollection(0);
 
 #
 
@@ -24,5 +26,7 @@ $f->{'morx'}->read();
 #print $f->{'morx'}->resolve_ligature([0x1f1ea, 0x1f1f9])."\n"; # some other flag
 #print $f->{'morx'}->resolve_ligature([0x1f3ca, 0x1f3ff])."\n"; # swimmer, black skin
 
-print $f->{'morx'}->resolve_ligature([0x1f469, 0x200d, 0x1f469, 0x200d, 0x1f467])."\n";
-print $f->{'morx'}->resolve_ligature([0x1f469, 0x200d, 0x1f469, 0x200d, 0x1f467, 0x200d, 0x1f466])."\n";
+#print $f->{'morx'}->resolve_ligature([0x1f469, 0x200d, 0x1f469, 0x200d, 0x1f467])."\n";
+#print $f->{'morx'}->resolve_ligature([0x1f469, 0x200d, 0x1f469, 0x200d, 0x1f467, 0x200d, 0x1f466])."\n";
+
+print Dumper $f->{'morx'}->resolve_ligature([0x1F468, 0x200D, 0x2764, 0xFE0F, 0x200D, 0x1F468]); # man-heart-man
