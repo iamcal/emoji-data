ab8365ee4a8b1099d18491c24caceabdfa5c99e6
Cal Henderson <cal@iamcal.com>
2021-06-08 16:56:56 +0000

    modified missing image tool to also show component images

--- build/apple/missing.php
+++ build/apple/missing.php
@@ -1,23 +1,28 @@
 <?php
-	$list = glob('../../img-apple-160/*_UNKNOWN.png');
 
-	$indexes = array();
+show_block("Components", '../../img-apple-160/*_COMPONENT.png');
+show_block("Unknowns", '../../img-apple-160/*_UNKNOWN.png');
 
-	foreach ($list as $name){
-		$parts = explode('/', $name);
-		list($idx) = explode('_', array_pop($parts));
-		$indexes[] = $idx;
-	}
+function show_block($title, $glob){
 
+	$list = glob($glob);
 
-	$chunks = array_chunk($indexes, 10);
+	$chunks = array_chunk($list, 10);
+
+	echo "<h1>{$title}</title>";
 
 	echo "<table border=1>";
 	foreach ($chunks as $chunk){
 		echo "<tr>";
-		foreach ($chunk as $idx){
-			echo "<td><img src=\"../../img-apple-160/{$idx}_UNKNOWN.png\" width=\"100\"><br>{$idx}</td>";
+		foreach ($chunk as $name){
+
+			$parts = explode('/', $name);
+			list($idx) = explode('_', array_pop($parts));
+
+			echo "<td><img src=\"$name\" width=\"100\"><br>{$idx}</td>";
 		}
 		echo "</tr>";
 	}
 	echo "</table>";
+
+}
