578ef50287131fa82cf33a83b16d1073bb716fa4
Cal Henderson <cal@iamcal.com>
2021-10-20 19:49:57 +0000

    added names for v14 emoji

--- build/build_map.php
+++ build/build_map.php
@@ -196,6 +196,7 @@
 	load_short_names('data_emoji_names_v12.txt');
 	load_short_names('data_emoji_names_v13.txt');
 	load_short_names('data_emoji_names_v13_1.txt');
+	load_short_names('data_emoji_names_v14.txt');
 	echo "DONE\n";
 
 	function load_short_names($file){
