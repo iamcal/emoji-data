d00bfddc0f4fee210c86d0b03201533223cc3cc0
Cal Henderson <cal@iamcal.com>
2023-04-17 21:17:13 +0000

    updated data files to 15.0

--- build/download_spec_files.sh
+++ build/download_spec_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget -O unicode/UnicodeData.txt		https://www.unicode.org/Public/14.0.0/ucd/UnicodeData.txt
-wget -O unicode/emoji-data.txt		https://www.unicode.org/Public/14.0.0/ucd/emoji/emoji-data.txt
-wget -O unicode/emoji-sequences.txt	https://www.unicode.org/Public/emoji/14.0/emoji-sequences.txt
-wget -O unicode/emoji-zwj-sequences.txt	https://www.unicode.org/Public/emoji/14.0/emoji-zwj-sequences.txt
-wget -O unicode/emoji-test.txt		https://www.unicode.org/Public/emoji/14.0/emoji-test.txt
+wget -O unicode/UnicodeData.txt		https://www.unicode.org/Public/15.0.0/ucd/UnicodeData.txt
+wget -O unicode/emoji-data.txt		https://www.unicode.org/Public/15.0.0/ucd/emoji/emoji-data.txt
+wget -O unicode/emoji-sequences.txt	https://www.unicode.org/Public/emoji/15.0/emoji-sequences.txt
+wget -O unicode/emoji-zwj-sequences.txt	https://www.unicode.org/Public/emoji/15.0/emoji-zwj-sequences.txt
+wget -O unicode/emoji-test.txt		https://www.unicode.org/Public/emoji/15.0/emoji-test.txt
